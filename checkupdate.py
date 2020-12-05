eff9ed5d1c35e029fa156b83cc87f72014aaf0d9
AMD794 <2952277346@qq.com>
2020-12-05 21:39:57 +0800

    update

--- checkupdate.py
+++ checkupdate.py
@@ -24,7 +24,7 @@ import os
 
 def get_response(url, error_file_path='.', max_count=5, timeout=15, encoding='utf-8', name=''):
     header = {
-        'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1 Edg/85.0.4183.83',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Mobile Safari/537.36 Edg/87.0.664.55',
     }
     count = 0
     while count < max_count:
